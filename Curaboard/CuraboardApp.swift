7b1cf517f4034c378ad28703fc36ef5a71691e67
bigjermaine <danieljermaine1@gmail.com>
2025-03-18 13:05:23 +0100

    Initial Commit

---
+++ Curaboard/CuraboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuraboardApp.swift
+//  Curaboard
+//
+//  Created by Daniel Jermaine on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CuraboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
