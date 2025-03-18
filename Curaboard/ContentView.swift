54b319c1d2d291d4facec81227917cf2f2e1da9e
bigjermaine <danieljermaine1@gmail.com>
2025-03-18 17:51:47 +0100

    started implemetation of Ui designs

--- Curaboard/ContentView.swift
+++ Curaboard/ContentView.swift
@@ -10,10 +10,54 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("curaBoardIcon")
+                .resizable()
+                .frame(width: 64, height: 64)
+            Text("Enable the Curaboard extension in Safari")
+                .foregroundStyle(.black)
+                .font(.system(size: 24, weight: .bold))
+                .multilineTextAlignment(.center)
+            Spacer()
+   
+                
+                VStack(alignment:.leading){
+                    HStack{
+                        Image("devicon_safari")
+                            .font(.system(size: 16, weight: .regular))
+                        Text("Open")
+                        
+                        +  Text(" ") + Text("Safari")
+                            .font(.system(size: 16, weight: .bold))
+                       
+                    }
+                    .foregroundStyle(.color575757)
+                    HStack{
+                        Image("mdi_extension-outline")
+                            .font(.system(size: 16, weight: .regular))
+                        Text("Tap on")
+                        
+                        +  Text(" ") + Text("Extensions")
+                            .font(.system(size: 16, weight: .bold))
+                      
+                    }
+                    .foregroundStyle(.color575757)
+                    HStack{
+                        Image("devicon_safari")
+                            .font(.system(size: 16, weight: .regular))
+                        Text("Open")
+                        
+                        +  Text(" ") + Text("Safari")
+                            .font(.system(size: 16, weight: .bold))
+                      
+                    }
+                    .foregroundStyle(.color575757)
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+               
+          
+            
+            Spacer()
         }
         .padding()
     }
