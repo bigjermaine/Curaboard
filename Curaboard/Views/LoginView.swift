468f8edb07dbcad5f651c266b4d214ab455803aa
bigjermaine <danieljermaine1@gmail.com>
2025-03-19 21:21:11 +0100

    feature(LoginView): started login screen

---
+++ Curaboard/Views/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  Curaboard
+//
+//  Created by Daniel Jermaine on 19/03/2025.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Text("Curaboard is ready!")
+        }
+      
+    }
+}
+
+#Preview {
+    LoginView()
+}
